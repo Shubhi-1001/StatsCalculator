516c9665b971b1ec508e4bdc43e8acddbb88984b
Shubhi-1001 <154509302+Shubhi-1001@users.noreply.github.com>
2024-02-18 20:29:13 +0530

    Mean_of_Dataset.py

--- New.py
+++ New.py
@@ -1,1 +1,18 @@
+def mean(data):
+  sum=0
+  n=0
+  for num in data:
+    sum=sum+num
+    n+=1
+  mean=sum/n
+  return mean
 
+def main():
+  user_input=input("Enter data points seperated by space: ")
+  ###user_input is a long chain of numbers in a string format###
+  Data_str=user_input.split(" ")
+  # for i in Data_str:
+  data=[int(i) for i in Data_str]
+  x=mean(data)
+  print("Mean is: ",x)
+  
